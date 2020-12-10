f70eaa9ae9747ab29d6fb37d5d5d34caf45a16a4
barryboubacar <barrybantignel@gmail.com>
2020-12-10 15:03:08 +0100

    Updates

---
+++ precache-manifest.0db568ab2b92ca4b351d68b8f3b7252c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41c76c7ed790f099b203359d1fcbb0b",
+    "url": "/stockageDenrees/index.html"
+  },
+  {
+    "revision": "414d52af32b44d7b2141",
+    "url": "/stockageDenrees/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8707d8e45ae7c88369a",
+    "url": "/stockageDenrees/static/js/2.fc40807c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stockageDenrees/static/js/2.fc40807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414d52af32b44d7b2141",
+    "url": "/stockageDenrees/static/js/main.0b912c6a.chunk.js"
+  },
+  {
+    "revision": "dac6e2b6bd1b7ed2b2ce",
+    "url": "/stockageDenrees/static/js/runtime-main.0d9cbef4.js"
+  }
+]);
